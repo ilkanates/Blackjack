17f559c8402a27da827fcd281dff3c4b68ce9c61
ilkanates <ilkanates@gmail.com>
2023-02-11 14:38:41 +0100

    Move card printing to deal_card

--- main.py
+++ main.py
@@ -40,12 +40,14 @@ def print_card(g):
 # Picks a random card from the deck and matching value from the dictionary
 # Pops the chosen card from the deck
 
-def deal_card(card_deck, cards):
+def deal_card(card_deck, cards, should_print=True):
     card = sample(card_deck, 1)
     value = card_deck_dct[card[0]]
     card_index = card_deck.index(card[0])
     card_deck.pop(card_index)
     cards.append(value)
+    if should_print:
+        print_card(card)
     return card, value
 
 
@@ -110,11 +112,10 @@ def win_lose_check(bet, player_cards, player_hand, wallet_value):
 
 def dealers_turn(card_deck, dealer_cards, dealer_hand, player_hand, status):
     while dealer_hand <= 16:
-        f = deal_card(card_deck, dealer_cards)
-        dealer_hand += f[1]
         print(f"Dealer received")
         sleep(0.5)
-        print_card(f[0])
+        f = deal_card(card_deck, dealer_cards)
+        dealer_hand += f[1]
         print(f"Dealer has {dealer_hand} points")
         sleep(0.5)
         if lose_check(dealer_hand):
@@ -146,20 +147,17 @@ def blackjack():
             print(f"You received")
             sleep(0.5)
             a = deal_card(card_deck, player_cards)
-            print_card(a[0])
             player_hand += a[1]
             b = deal_card(card_deck, player_cards)
             player_hand += b[1]
-            print_card(b[0])
             print(f"You have {player_hand} points")
             sleep(0.5)
 
             print(f"Dealer has")
             sleep(0.5)
             c = deal_card(card_deck, dealer_cards)
-            print_card(c[0])
             dealer_hand += c[1]
-            d = deal_card(card_deck, dealer_cards)
+            d = deal_card(card_deck, dealer_cards, False)
             dealer_hand += d[1]
 
             if win_check(player_hand):
@@ -171,11 +169,10 @@ def blackjack():
             while True:
                 new_card = input("Do you want another card? Y/N ")
                 if new_card.lower() == "y":
-                    e = deal_card(card_deck, player_cards)
-                    player_hand += e[1]
                     print(f"You received")
                     sleep(0.5)
-                    print_card(e[0])
+                    e = deal_card(card_deck, player_cards)
+                    player_hand += e[1]
                     print(f"You have {player_hand} points")
                     sleep(0.5)
 
